a7e05c8ae413cf4f8ac801a378086bd7fdc7a793
henriquesml <henrique_schmeller@hotmail.com>
2019-12-10 16:35:22 -0300

    Usando o FactoryBot  nos testes

--- Rails_tests/spec/models/user_spec.rb
+++ Rails_tests/spec/models/user_spec.rb
@@ -2,6 +2,30 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is invalid if the level is not between 1 and 99" do
+
+    # Gerando um um User com level maior que o permitido
+    user = build(:user, level: FFaker::Random.rand(100..9999))
+    expect(user).to_not be_valid
+  end
+
+  it "return the correct hero title" do
+    nickname = FFaker::Name.first_name
+    kind = %i[knight wizard].sample
+    level = FFaker::Random.rand(1..99)
+
+    user = create(:user, nickname: nickname, kind: kind, level: level )
+    expect(user.title).to eq("#{kind} #{nickname} ##{level}")
+
+  end
+
+end
+
+=begin
+
+# TESTE SEM FACTORY_BOT
+
+RSpec.describe User, type: :model do
+  it "is invalid if the level is not between 1 and 99" do
     nickname = FFaker::Name.first_name
     kind = %i[knight wizard].sample
     level = FFaker::Random.rand(100..9999)
@@ -21,4 +45,5 @@ RSpec.describe User, type: :model do
   end
 
 end
-
+  
+=end
